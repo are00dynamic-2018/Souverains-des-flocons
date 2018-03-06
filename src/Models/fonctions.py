23a31b452a0dcb5ceeb547ff09226c396e05f80e
melx1998 <36272739+melx1998@users.noreply.github.com>
2018-03-07 00:22:29 +0100

    Update fonctions.py

--- src/Models/fonctions.py
+++ src/Models/fonctions.py
@@ -1,6 +1,5 @@
 #réaliser une fonction : centrale, edge. la fonction centrale remplace le bool en argument (a supprimer) teste si une cellule est la cellule centrale, idem pour edge.
 #Neighbors : determine les voisisns d'une celleule (prend un tuple de coordonnees en argument)
-#NeighborsAverage : prend en argument une cellule et calcule la moyenne de ses 6 voisins
 
 def Init(beta, mat): #beta : background vapor level
     """float*array*bool->None
@@ -35,13 +34,16 @@ def NeighborsAverage(cell, mat):
             S+=Neighbors(k, l)
     return S/6
             
-def UpdateState(mat, alpha, gamma): #alpha : constante de diffusion, gamma : constante ajoutee (eau provenant d'autres cellules)
+def UpdateState(mat, alpha, beta, gamma): #alpha : constante de diffusion, gamma : constante ajoutee (eau provenant d'autres cellules)
     """array*float*float-> None
     Met a jour l'etat et la valeur de chaque cellule"""
     for i in mat[i, j]:
         for j in mat[i, j]:
-            if Receptive(mat[i, j], mat):
+            if Edge(mat[i, j], mat):
+                mat[i, j]=beta
+            elif Receptive(mat[i, j], mat):
                 mat[i, j]=(NeighborsAverage(mat[i,j])+alpha/12*NeighborsAverage(mat[i,j]))+(mat[i, j]+gamma) #on factorisera plus tard
                 #on additionne partie participant a la diffusion + partie non diffusion
             else :
                 mat[i, j]=(NeighborsAverage(mat[i,j])+alpha/12*NeighborsAverage(mat[i,j])-mat[i, j])
+
