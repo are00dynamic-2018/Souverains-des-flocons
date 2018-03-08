3d474d84c90be16a1a8a79da7a562117710cd14e
Mistergix <nicolas.ruche.entreprise@gmail.com>
2018-03-08 15:07:04 +0100

    MODEL

--- src/Models/model.py
+++ src/Models/model.py
@@ -1,8 +1,18 @@
-import numpy as np
+#import numpy as np
 
 class Model:
     def __init__(self):
         pass
 
     def updateGrid(self, hexaGrid):
+        """
+        HexaGrid -> void
+        """
         pass
+
+    def getNeighbors(self, hexaGrid, ijk):
+        """
+        HexaGrid -> (int, int, int)
+        """
+        pass
+
