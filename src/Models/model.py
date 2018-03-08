2b6459307d8281a54f1b5fb75ade3cd714641f49
Mistergix <nicolas.ruche.entreprise@gmail.com>
2018-03-08 16:47:49 +0100

    MODEL

--- src/Models/model.py
+++ src/Models/model.py
@@ -2,7 +2,7 @@ import numpy as np
 from .Controllers.hexaGrid import *
 
 class Model:
-    def __init__(self, alpha, beta, gamma):
+    def __init__(self, hexaGrid, alpha, beta, gamma):
         """
         alpha : float (0 <= b <= 1) constante de diffusion
         beta : float (0 <= b <= 1) background vapor level
@@ -17,22 +17,42 @@ class Model:
         self.beta = beta
         self.gamma = gamma
 
-    def InitHexaGrid(self, hexaGrid):
+        self.hexaGrid = hexaGrid
+
+    def InitHexaGrid(self):
         """
-        HexaGrid -> void
+        void -> void
         Initialise la grille : toutes les cellules à beta sauf la cellule centrale à 1
         """
-        I,J,K = hexaGrid.getSize()
+        I,J,K = self.hexaGrid.getSize()
         for i in range(I):
             for j in range(J):
                 for k in range(K):
-                    hexaGrid[i,j,k] = if (i == I//2 and j == J//2 and k == K//2) 1 else self.beta
+                    self.hexaGrid[i,j,k] = if (i == I//2 and j == J//2 and k == K//2) 1 else self.beta
 
-    def UpdateGrid(self, hexaGrid):
+    def UpdateGrid(self):
         """
-        HexaGrid -> void
+        void -> void
         Met à jour la grille
         """
         pass
+
+    def __Receptive(self, hexaCell):
+        """
+        HexaCell -> bool
+        Renvoit True si la cellule est glacée ou a un voisin glacé
+        """
+        if hexaCell.data >= 1:
+            return True
+
+        #neighborCell : HexaCell
+        for neighborCell in self.hexaGrid.getNeighbors(hexaCell.ijk):
+            if neighborCell.data >= 1 :
+                return True
+
+        return False
+
+    
+             
 
 m = Model(0.2, 0.3)
