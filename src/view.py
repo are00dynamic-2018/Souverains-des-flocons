7367c769fed551fde6409d0e313d2576754722bb
Mistergix <nicolas.ruche.entreprise@gmail.com>
2018-03-15 17:06:38 +0100

    View

--- src/view.py
+++ src/view.py
@@ -1,20 +1,23 @@
 from tkinter import *
+from controller import *
 
 class Window :
-    def __init__(self):
+    def __init__(self, t_ijk):
+        self.controller = None
         self._InitUI()
 
     def _InitUI(self):
         """
         Creates the windows and displays it
         """
+        # Initialisation
         self.window = Tk()
         self.canvas = Canvas(self.window)
         
         self.window.title('fenetre')
 
-        btn1 = Button(self.window, text="Next Step", command=self.NextStep)
-        btn2 = Button(self.window, text="Reset", command=self.ResetGrid)
+        btn1 = Button(self.window, text="Next Step", command=self._NextStep)
+        btn2 = Button(self.window, text="Reset", command=self._ResetGrid)
 
         self.buttons = dict(nextStep=btn1, reset=btn2)
         
@@ -28,10 +31,19 @@ class Window :
         gamma = Scale(self.window, orient='horizontal', from_=0, to=1, resolution=0.1, tickinterval=2, length=350, label='Gamma')
       
         self.sliders = dict(alpha=alpha, beta=beta, gamma=gamma)
+        
+        # Positionnement
+        self.ResetGrid()
+        self.window.mainloop()
 
-    def ResetGrid(self):
-        pass
+    def _ResetGrid(self):
+        alpha = self.sliders.alpha.get()
+        beta = self.sliders.beta.get()
+        gamma = self.sliders.gamma.get()
+        self.controller = Controller(self.t_ijk, alpha, beta, gamma)
 
-    def NextStep(self):
-        pass
+        self.controller.ResetGrid()
+
+    def _NextStep(self):
+        assert self.controller != None, "La grille n'est pas initialisée : appuyer sur Reset"
       
