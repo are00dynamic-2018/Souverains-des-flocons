6698b6c5cb1846cd17fc22532c1726f07e5eb1da
Mistergix <nicolas.ruche.entreprise@gmail.com>
2018-04-11 18:09:04 +0200

    Latex

--- src/view.py
+++ src/view.py
@@ -135,7 +135,7 @@ class Window:
 
         alpha = Scale(mdl, orient='horizontal', from_=0, to=3, resolution=0.1, tickinterval=2, label='Alpha', command=self._needReset)
         beta = Scale(mdl, orient='horizontal', from_=0, to=1, resolution=0.1, tickinterval=2, label='Beta', command=self._needReset)
-        gamma = Scale(mdl, orient='horizontal', from_=0, to=0.05, resolution=-1, tickinterval=2, label='Gamma', command=self._needReset)
+        gamma = Scale(mdl, orient='horizontal', from_=0, to=1, resolution=0.00001, tickinterval=2, label='Gamma', command=self._needReset)
         
         #init values:
         cm = self.controller.model
@@ -287,9 +287,9 @@ class Window:
                                 
                         #add text for step:
                         try:
-                            self.canvas.itemconfig(self.canvas_cells["txt"], text="t = {}".format(step))
+                            self.canvas.itemconfig(self.canvas_cells["txt"], text="t = {}\nbeta = {}\ngamma = {}".format(step, cm.beta, cm.gamma))
                         except KeyError:
-                            self.canvas_cells["txt"] = self.canvas.create_text(1, 1, anchor=NW, text="t = 0", fill="white")
+                            self.canvas_cells["txt"] = self.canvas.create_text(1, 1, anchor=NW, text="t = 0\nbeta = {}\ngamma = {}".format(cm.beta, cm.gamma), fill="white")
                                 
                         self.canvas.update()
                         working = False
